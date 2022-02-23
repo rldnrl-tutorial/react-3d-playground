58c4240898ccd5eee05a7cee695400e9d078b000
rldnrl <devpark1002@gmail.com>
2022-02-23 21:15:09 +0900

    feat: wip earth

---
+++ apps/earth/src/components/earth.tsx
@@ -0,0 +1,36 @@
+import { TextureLoader } from 'three'
+import { useLoader } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+import EarthDayMap from '../assets/textures/8k_earth_daymap.jpg'
+import EarthNormalMap from '../assets/textures/8k_earth_normal_map.jpg'
+import EarthSpecularMap from '../assets/textures/8k_earth_specular_map.jpg'
+import EarthCloudsMap from '../assets/textures/8k_earth_clouds.jpg'
+
+const Earth = () => {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  )
+
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <mesh>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
+      </mesh>
+    </>
+  )
+}
+
+export default Earth
