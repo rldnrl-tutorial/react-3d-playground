995c2a3dc1620296d2fba31ed7f73f958f292558
rldnrl <devpark1002@gmail.com>
2022-02-23 21:14:49 +0900

    feat: set up for 3d

--- apps/earth/src/app/app.tsx
+++ apps/earth/src/app/app.tsx
@@ -1,16 +1,23 @@
-import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
+import { css } from '@emotion/react'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Earth from '../components/earth'
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+const canvasStyleContainer = css`
+  width: 100%;
+  height: 100%;
+`
 
-export function App() {
+const App = () => {
   return (
-    <StyledApp>
-      <NxWelcome title="earth" />
-    </StyledApp>
-  );
+    <div css={canvasStyleContainer}>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
 }
 
-export default App;
+export default App
